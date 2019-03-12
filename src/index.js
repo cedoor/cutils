c6927afcb4e367477bf2d8e8ea13e41d13735171
Omar Desogus <omardesogus9@gmail.com>
2019-03-12 16:00:38 +0100

    [refactor] small syntax improvements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { default as strings } from './strings/index'
+export { default as scraping } from './scraping/index'
+export { default as file } from './file/index'
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const tape = require('tape')
 const cutls = require('../dist/cutls')
 
-tape('timing test', function (test) {
+tape('Strings module tests', function (test) {
   test.equal(cutls.strings.capitalizeWord('hello'), 'Hello')
   test.equal(cutls.strings.capitalizeWords('hello world'), 'Hello World')
   test.deepEqual(cutls.strings.capitalizeWordList(['hello', 'world']), ['Hello', 'World'])
