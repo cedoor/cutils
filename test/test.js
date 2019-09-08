2f71320051d7ce30156f953661d03ee65aadc454
cedoor <omardesogus9@gmail.com>
2019-09-08 18:57:26 +0200

    [test] add tests for parseURL function

--- test/test.js
+++ test/test.js
@@ -4,10 +4,24 @@ const utils = require('../dist/utils')
 /** String module tests **/
 
 tape('Capitalize functions', function (test) {
-  test.equal(utils.strings.capitalize('hello'), 'Hello')
-  test.deepEqual(utils.strings.capitalizeList(['hello', 'world']), ['Hello', 'World'])
-  test.throws(() => utils.strings.capitalize(1))
-  test.throws(() => utils.strings.capitalize(true))
-  test.throws(() => utils.strings.capitalizeList(['hello', 1]))
+  test.equal(utils.capitalize('hello'), 'Hello')
+  test.deepEqual(utils.capitalizeList(['hello', 'world']), ['Hello', 'World'])
+
+  test.throws(() => utils.capitalize(1))
+  test.throws(() => utils.capitalize(true))
+  test.throws(() => utils.capitalizeList(['hello', 1]))
+
+  test.end()
+})
+
+tape('ParseURL function', function (test) {
+  const parsedURL = utils.parseURL('http://example.com:4200?a=1&b=2')
+  const parsedURLParams = Array.from(parsedURL.params)
+
+  test.equal(parsedURL.hostname, 'example.com')
+  test.equal(parsedURL.port, '4200')
+  test.equal(parsedURL.protocol, 'http')
+  test.deepEqual(parsedURLParams, [['a', '1'], ['b', '2']])
+
   test.end()
 })
